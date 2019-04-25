92029f95a236babac53c6afa970019412deb9c8a
arrowGE <49671997+arrowGE@users.noreply.github.com>
2019-04-25 15:50:46 +0900

    Update check.c

--- Jikken2/claret/check.c
+++ Jikken2/claret/check.c
@@ -4,7 +4,88 @@
 #include <sys/time.h>
 #include <values.h>
 
-#include "fastexpf.c"
+#include <values.h>
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+static float expTbl0[128 + 1];
+#define d0MaskLen 12
+static float expTbl1[1 << d0MaskLen];
+
+#define NUM_OF_ARRAY(x) (sizeof(x) / sizeof(*x))
+#define mask(x) ((1U << (x)) - 1)
+
+union fi {
+  float f;
+  unsigned int i;
+};
+
+void InitExp()
+{
+  size_t i;
+  for (i = 0; i < NUM_OF_ARRAY(expTbl0); i++)
+  {
+    expTbl0[i] = expf((float)i - 1);
+  }
+  for (i = 0; i < (1U << d0MaskLen); i++)
+  {
+    union fi fi;
+    fi.i = (127 << 23) | (i << 11);
+    expTbl1[i] = expf(fi.f);
+  }
+}
+
+/*
+  log(FLT_MAX) = 88.7 then return FLT_MAX if floor(x) >= 89
+*/
+
+
+__inline__ float Exp(float x)
+{
+#if 1
+  int isPositive = 0;
+  if (x >= 0)
+  {
+    isPositive = 1;
+  }
+  else
+  {
+    x = -x;
+    isPositive = 0;
+  }
+#endif
+  int n = (int)floor(x) - 1;
+  float e;
+
+  if (n >= 88)
+    e = FLT_MAX;
+  else
+  {
+    float d = x - n;
+
+    union fi fi;
+    fi.f = d;
+    unsigned int di = fi.i;
+
+    /*
+      sign + exponent + mantissa
+      [1]      [8]      [12][11]
+      d0  d1
+    */
+    int d0i = (di >> 11) & mask(12);
+
+    fi.i = di & ((mask(8) << 23) | mask(11));
+    float d1 = fi.f;
+    e = expTbl0[n + 1] * expTbl1[d0i] * d1;
+  }
+#if 1
+  if (!isPositive)
+  {
+    e = 1 / e;
+  }
+#endif
+  return e;
+}
 
 static void get_cputime(double *laptime, double *sprittime)
 {
